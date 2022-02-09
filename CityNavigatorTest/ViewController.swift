0d570fc61dbf6a60ad1a5d6acbdbb7c4190b5465
Никита <81251222+NikitaKelevra@users.noreply.github.com>
2022-02-09 18:18:17 +0300

    Initial Commit

---
+++ CityNavigatorTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CityNavigatorTest
+//
+//  Created by Сперанский Никита on 09.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
